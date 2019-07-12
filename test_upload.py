7dacba85740287f16d3650417ccfd235e428682e
Shiva Tripathi <50612524+shiva-t@users.noreply.github.com>
2019-07-12 23:20:24 +0530

    Delete test_upload.py

--- test_upload.py
+++
@@ -1,8 +0,0 @@
-from google.cloud import storage
-
-def uploadFile(file):
- storage_client = storage.Client()
- bucket = storage_client.get_bucket('video-surv.appspot.com')
- blob = bucket.blob(file)
- blob.upload_from_filename(file)
-#print('File {} uploaded to {}'.format(file,file))
