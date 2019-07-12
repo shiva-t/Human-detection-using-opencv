523de58cd4c199d107aefaccf18212cf5d6b3ab9
Shiva Tripathi <50612524+shiva-t@users.noreply.github.com>
2019-07-12 23:20:37 +0530

    Delete test_del.py

--- test_del.py
+++
@@ -1,7 +0,0 @@
-from google.cloud import storage
-
-storage_client = storage.Client()
-bucket = storage_client.get_bucket('shetty_buck')
-bucket.delete()
-print('File {} uploaded to {}'.format('shetty_buck'))
-
