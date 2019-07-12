81e5301d66ebdff11f336e9e84a638167a918de1
Shiva Tripathi <50612524+shiva-t@users.noreply.github.com>
2019-07-12 23:19:45 +0530

    Delete test_bucket.py

--- test_bucket.py
+++
@@ -1,6 +0,0 @@
-from google.cloud import storage
-
-storage_client = storage.Client()
-bucket_name='shetty_buck'
-bucket=storage_client.create_bucket(bucket_name)
-print('Bucket {} created .'.format(bucket.name))
