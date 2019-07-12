07c35371531fabaea5463f550c65c1b5c80ee23d
Shiva Tripathi <50612524+shiva-t@users.noreply.github.com>
2019-07-12 23:20:52 +0530

    Delete data_store.py

--- data_store.py
+++
@@ -1,9 +0,0 @@
-from google.cloud import datastore
-
-datastore_client = datastore.Client()
-task_key = datastore_client.key('Task',5634472569470976)
-task_entity = datastore_client.get(task_key)
-print(task_key)
-print(task_entity)
-print(task_entity['Done'])
-
