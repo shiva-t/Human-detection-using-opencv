3b1bab98dbc44da2a1865a731bf2f6b7a07114d6
Shiva Tripathi <50612524+shiva-t@users.noreply.github.com>
2019-07-12 23:19:59 +0530

    Delete test_firestore.py

--- test_firestore.py
+++
@@ -1,9 +0,0 @@
-from google.cloud import firestore
-
-def getFlag():
- db = firestore.Client()
- collec = db.collection('switch')
- docs = collec.get()
- for doc in docs:
-  flag = doc.get('status')
- return flag
